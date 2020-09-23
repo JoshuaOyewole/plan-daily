4b03dae4c90400c32d04eedda71b01f4d75cd342
Joshua Oyewole <joshpp2013@gmail.com>
2020-09-23 22:42:15 +0100

    dependencies issues fixed in RandomQuotes Component

--- src/Components/RandomQuotes.js
+++ src/Components/RandomQuotes.js
@@ -3,16 +3,17 @@ import React, {useState, useEffect} from 'react'
 function RandomQuotes() {
     const [quotes, setQuotes] = useState([]);
 
-    const randomNo = Math.floor(Math.random() * 1643);
+   
 
     useEffect(()=>{
-  
+        const randomNo = Math.floor(Math.random() * 1643);
+
         fetch('https://type.fit/api/quotes')
         .then(quotes=> {return quotes.json()})
         .then(data => {return setQuotes(data[randomNo])})
         .catch(err =>{console.log('Unable to fetch data:' + err);
         });   
-    },[randomNo])
+    },[])
    
 
     return (
